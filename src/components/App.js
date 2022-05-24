45e56711c8a89a2a2522f030bc654fd5709d02bc
Shiwani-325 <shrivastavashivani325@gmail.com>
2022-05-24 22:41:26 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 const App = () => {
-  return (
-    <div id="main"></div>
-  )
+    return ( <
+        div id = "main" >
+        <
+        h1 data - ns - test = "project-name" > Amazon Clone < /h1> <
+        p data - ns - test = "project-description" > Amazon clone in which user can register and login, see the products list, add to cart and remove from the cart when required < /p> <
+        /div>
+    )
 }
 
 
